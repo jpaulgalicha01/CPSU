922cd6be059c933202f8d73b22f75d9343f2ec37
Jpaulgalicha01 <jpaulgalicha01@gmail.com>
2024-08-30 11:35:38 +0800

    logout

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+include 'includes/autoload.inc.php';
+
+session_unset();
+session_destroy();
+
+setcookie("UserID",NULL, time()-3600, '/CPSU-main/CPSU');
+setcookie("TypeUser",NULL, time()-3600, '/CPSU-main/CPSU');
+ob_end_flush(header("Location: ".$_SERVER['HTTP_REFERER'].""));
+?>
